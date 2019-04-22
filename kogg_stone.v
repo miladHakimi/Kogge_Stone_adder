0e2451928ba6af13f9365eea616169b876795661
miladHakimi <miladhakimi33@gmail.com>
2019-04-22 20:25:38 +0430

    code volume reduced :)

--- kogg_stone.v
+++ kogg_stone.v
@@ -19,35 +19,16 @@ assign init_g = (data1 & data2);
 assign {black_cell_input_g_i_to_k[13:0], black_cell_input_p_i_to_k[13:0]} = {init_g[15:2], init_p[15:2]};
 assign {black_cell_input_g_k_to_j[13:0], black_cell_input_p_k_to_j[13:0]} = {init_g[14:1], init_p[14:1]};
 
-assign black_cell_input_g_i_to_k[25:14] = black_cell_output_g_i_to_k[13:2];
-assign black_cell_input_p_i_to_k[25:14] = black_cell_output_p_i_to_k[13:2];
-assign black_cell_input_g_i_to_k[33:26] = black_cell_output_g_i_to_k[25:18];
-assign black_cell_input_p_i_to_k[33:26] = black_cell_output_p_i_to_k[25:18];
+assign black_cell_input_g_i_to_k[33:14] = {black_cell_output_g_i_to_k[25:18], black_cell_output_g_i_to_k[13:2]};
+assign black_cell_input_p_i_to_k[33:14] = {black_cell_output_p_i_to_k[25:18], black_cell_output_p_i_to_k[13:2]};
 
-assign black_cell_input_p_k_to_j[25:14] = black_cell_output_g_i_to_k[11:0];
-assign black_cell_input_p_k_to_j[25:14] = black_cell_output_p_i_to_k[11:0];
-assign black_cell_input_g_k_to_j[33:26] = black_cell_output_g_i_to_k[21:14];
-assign black_cell_input_p_k_to_j[33:26] = black_cell_output_p_i_to_k[21:14];
+assign black_cell_input_g_k_to_j[33:26] = {black_cell_output_g_i_to_k[21:14], black_cell_output_g_i_to_k[11:0]};
+assign black_cell_input_p_k_to_j[33:26] = {black_cell_output_p_i_to_k[21:14], black_cell_output_p_i_to_k[11:0]};
 
-assign gray_cell_input_g_i_to_k[14:7] = black_cell_output_g_i_to_k[33:26];
-assign gray_cell_input_p_i_to_k[14:7] = black_cell_output_p_i_to_k[33:26];
+assign gray_cell_input_g_i_to_k[14:0] = {black_cell_output_g_i_to_k[33:26], black_cell_output_g_i_to_k[17:14], black_cell_output_g_i_to_k[1:0], init_g[1]};
+assign gray_cell_input_p_i_to_k[14:0] = {black_cell_output_p_i_to_k[33:26], black_cell_output_p_i_to_k[17:14], black_cell_output_p_i_to_k[1:0], init_p[1]};
 
-assign gray_cell_input_g_i_to_k[6:3] = black_cell_output_g_i_to_k[17:14];
-assign gray_cell_input_p_i_to_k[6:3] = black_cell_output_p_i_to_k[17:14];
-
-assign gray_cell_input_g_i_to_k[2:1] = black_cell_output_g_i_to_k[1:0];
-assign gray_cell_input_p_i_to_k[2:1] = black_cell_output_p_i_to_k[1:0];
-
-assign gray_cell_input_g_i_to_k[0] = init_g[1];
-assign gray_cell_input_p_i_to_k[0] = init_p[1];
-
-assign gray_cell_input_g_k_to_j[14:11] = gray_cell_output_g[6:3];
-assign gray_cell_input_g_k_to_j[10:7] = bufferd_output[3:0];
-assign gray_cell_input_g_k_to_j[6:5] = gray_cell_output_g[2:1];
-assign gray_cell_input_g_k_to_j[4:3] = bufferd_output[1:0];
-assign gray_cell_input_g_k_to_j[2:1] = {gray_cell_output_g[0], bufferd_output[0]};
-assign gray_cell_input_g_k_to_j[0] = init_g[0];
-
+assign gray_cell_input_g_k_to_j[14:0] = {gray_cell_output_g[6:3], bufferd_output[3:0], gray_cell_output_g[2:1], bufferd_output[1:0], gray_cell_output_g[0], bufferd_output[0], init_g[0]};
 assign output_g = {black_cell_output_g_i_to_k[33:26], bufferd_output};
 
 //buffer
